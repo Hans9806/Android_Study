6c775a9784131e3c0bfdfac2cfcd6b1a69f58348
Hans9806 <gchan9806@gmail.com>
2024-05-14 16:29:26 +0900

    안드로이드 리소스

---
+++ ch03_resource/src/main/java/com/busanit/ch03_resource/Resource5Activity.kt
@@ -0,0 +1,15 @@
+package com.busanit.ch03_resource
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Resource5Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_resource5)
+    }
+}
