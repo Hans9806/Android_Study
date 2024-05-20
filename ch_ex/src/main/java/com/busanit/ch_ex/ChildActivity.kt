5714a50fba9a6ab21030d268407f5369bda0baab
Hans9806 <gchan9806@gmail.com>
2024-05-20 16:40:06 +0900

    appbar (toolbar) 연습문제

--- ch_ex/src/main/java/com/busanit/ch_ex/ChildActivity.kt
+++ ch_ex/src/main/java/com/busanit/ch_ex/ChildActivity.kt
@@ -2,6 +2,7 @@ package com.busanit.ch_ex
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,19 +10,22 @@ import androidx.core.view.WindowInsetsCompat
 import com.busanit.ch_ex.databinding.ActivityChildBinding
 
 class ChildActivity : AppCompatActivity() {
+    lateinit var binding: ActivityChildBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityChildBinding.inflate(layoutInflater)
+        binding = ActivityChildBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 업버튼 활성화
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
+                // 업버튼 클릭시 이벤트 활성화
+                Toast.makeText(this, "home 업버튼 선택", Toast.LENGTH_SHORT).show()
                 finish()
-                return true
             }
         }
         return super.onOptionsItemSelected(item)
--- ch_ex/src/main/java/com/busanit/ch_ex/Main1Activity.kt
+++ ch_ex/src/main/java/com/busanit/ch_ex/Main1Activity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,32 +12,37 @@ import androidx.core.view.WindowInsetsCompat
 import com.busanit.ch_ex.databinding.ActivityMain1Binding
 
 class Main1Activity : AppCompatActivity() {
+    lateinit var binding: ActivityMain1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMain1Binding.inflate(layoutInflater)
+        binding = ActivityMain1Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            Intent(this, ChildActivity::class.java)
-            startActivity(intent)
-        }
+        setSupportActionBar(binding.toolbar)
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // XML 의 메뉴를 inflate 해줌
         menuInflater.inflate(R.menu.menu1, menu)
-        return true
+        return super.onCreateOptionsMenu(menu)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.menu_search -> {
-                // Search 메뉴 항목 클릭 시 처리할 작업
+            R.id.search -> {
+                Toast.makeText(this, "search 선택", Toast.LENGTH_SHORT).show()
+
+                // 메뉴 선택시 액티비티 전환
+                val intent = Intent(this, ChildActivity::class.java)
+                startActivity(intent)
                 return true
             }
-            R.id.menu_settings -> {
-                // Settings 메뉴 항목 클릭 시 처리할 작업
+            R.id.settings -> {
+                Toast.makeText(this, "settings 선택", Toast.LENGTH_SHORT).show()
                 return true
             }
-            else -> return super.onOptionsItemSelected(item)
         }
+        return super.onOptionsItemSelected(item)
     }
 }
